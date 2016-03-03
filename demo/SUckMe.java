356ab0a2c5154257d6ef055cb00037545b58e612
haha suck me <Uve Been Hacked @gmail.com>
2016-03-03 14:15:18 -0600

    suck me

---
+++ demo/SUckMe.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class SUckMe here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SUckMe
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SUckMe
+     */
+    public SUckMe()
+    {
+        // initialise instance variables
+        x = 0;
+        System.out.println("haha suck me taetae");
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
