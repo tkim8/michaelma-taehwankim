430810ee7c43c88bdddb9f28956bbeee8ea282b3
Taehwan Kim <tkim8@students.d125.org>
2016-03-03 14:15:34 -0600

    Taehwan Kim

--- demo/demo.java
+++ demo/demo.java
@@ -9,22 +9,6 @@ public class demo
 {
     public static void main(String [] args)
     {
-      ArrayList<String> al = new ArrayList<String>();
-      al.add("C");
-      al.add("A");
-      al.add("E");
-      al.add("B");
-      al.add("D");
-      al.add("F");
-      Iterator iter= al.iterator();
-      
-      iter.next();
-      iter.remove();
-      iter.next();
-      iter.remove();
-      iter.remove();
-      iter.next();
-      iter.remove();
-      System.out.println(al);
+       System.out.print("Michael is a complete weirdo");
     }
 }
